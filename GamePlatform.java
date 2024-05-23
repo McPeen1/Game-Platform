9a46c3fec8efbe9ad2c074eef395ad1b2749c3c9
McPeen1 <17anryshpa@gmail.com>
2024-05-23 17:48:43 +0000

    Imported the new code to the main

--- GamePlatform.java
+++ GamePlatform.java
@@ -1,15 +1,15 @@
-import java.util.Scanner;
-import EightBall.EightBall;
+import java.util.Scanner; 
+import EightBall.EightBall; //imported Magic EightBall
 public class GamePlatform {
     public static void main(String[] args) {
         Scanner game = new Scanner(System.in);
-
+ 
         System.out.println("Welcome to the minigame choices!");
         System.out.println("Choose a game to play (1-3): ");
         int choice = game.nextInt(); /// Having 3 different game choices to choose from
         
-        if (choice ==1) {1
-            System.out.println("1. Game 1");
+        if (choice ==1) {
+            System.out.println("1. Game 1"); ///game 1  Magic Eightball Random Responses
             EightBall.main(args);
         }
         else if (choice == 2) {
@@ -19,7 +19,7 @@ public class GamePlatform {
     System.out.println("3. Game 3"); 
     }
     else{
-        System.out.println("Invalid choice! Please try again");
+        System.out.println("Invalid choice! Please try again"); ///if you dont pick a choice 1-3 it would print out Inavlid choice
     }
     game.close();
     }
